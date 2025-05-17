1f57fc9d25cafaaffdad9012bbf9e45e4e20284d
Danilo Aparecido <danilo.aparecido.santos@gmail.com>
2025-05-17 10:23:47 -0300

    pagina esqueci senha

--- push.sh
+++ push.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-read -p "Digite a mensagem do commit: " mensagem
+mensagem="$1"
+
+if [ -z "$mensagem" ]; then
+    echo "Por favor forneça uma mensagem de commit como parâmetro"
+    echo "Uso: ./push.sh \"sua mensagem de commit\""
+    exit 1
+fi
 
 git add .
-git commit -am $mensagem
+git commit -am "$mensagem"
 
 git push
 
