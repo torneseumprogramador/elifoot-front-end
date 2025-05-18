d9d00c2ddace9566e15964a9865cfa5338afb5b8
Danilo Aparecido <danilo.aparecido.santos@gmail.com>
2025-05-18 13:42:20 -0300

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base API configuration
 export const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL || ""}/api`,
   headers: {
     "Content-Type": "application/json",
   },
